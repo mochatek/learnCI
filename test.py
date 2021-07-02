20c5545cece981ec236c74770d224b13a83e060a
mochatek <akashsabu@ymail.com>
2021-07-02 10:14:09 +0530

    test.py modified

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
 import unittest
 from os import path
 
-ROOT = path.dirname(path.abspath(__file__))
-
 
 class TestPackage(unittest.TestCase):
     def test_build(self):
         # Log file will be created if app ran
-        log_path = path.join(ROOT, 'dist', 'error.log')
+        log_path = path.join('dist', 'error.log')
         self.assertTrue(path.exists(log_path))
 
         # Log file will be empty if app ran without any error
